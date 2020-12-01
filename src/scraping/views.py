87bc293395739e816a1dcdbfebd8b5b086f35d0b
dbv111m <dbv111m@gmail.com>
2020-12-01 07:36:56 +0300

    Форма, как встроенный класс в Django

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
+
+from .forms import FindForm
 from .models import Vacancy
 
 # Create your views here.
 
 def home_view(request):
     # print(request.GET) # возвращает словарь
+    form = FindForm()
     city=request.GET.get('city')
     language=request.GET.get('language')
     qs=[]
     if city or language:
         _filter={}
         if city:
-            _filter['city__name'] = city # в Django обращение идет через __
+            _filter['city__slug'] = city # в Django обращение идет через __
         if language:
-            _filter['language__name'] = language # в Django обращение идет через __
+            _filter['language__slug'] = language # в Django обращение идет через __
 
 
         qs = Vacancy.objects.filter(**_filter)
 
-    return render(request, 'scraping/home.html', {'object_list': qs})
+    return render(request, 'scraping/home.html', {'object_list': qs, 'form': form})
