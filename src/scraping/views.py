7bc2e4179c31a3dfa4135c6e23bc80c1452bcf52
dbv111m <dbv111m@gmail.com>
2020-12-01 05:48:57 +0300

    Поиск по параметрам из формы

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -4,7 +4,18 @@ from .models import Vacancy
 # Create your views here.
 
 def home_view(request):
-    print(request.POST)
-    qs = Vacancy.objects.all()
+    # print(request.GET) # возвращает словарь
+    city=request.GET.get('city')
+    language=request.GET.get('language')
+    qs=[]
+    if city or language:
+        _filter={}
+        if city:
+            _filter['city__name'] = city # в Django обращение идет через __
+        if language:
+            _filter['language__name'] = language # в Django обращение идет через __
+
+
+        qs = Vacancy.objects.filter(**_filter)
 
     return render(request, 'scraping/home.html', {'object_list': qs})
