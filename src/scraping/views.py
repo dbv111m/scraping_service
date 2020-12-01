b79590cde685ccb1a35aed83472619fd5f151af4
dbv111m <dbv111m@gmail.com>
2020-12-01 04:10:27 +0300

    Добавление HTML формы запроса

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -4,6 +4,7 @@ from .models import Vacancy
 # Create your views here.
 
 def home_view(request):
+    print(request.POST)
     qs = Vacancy.objects.all()
 
     return render(request, 'scraping/home.html', {'object_list': qs})
