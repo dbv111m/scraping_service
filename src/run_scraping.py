6c76a446212cdff1ae95fd24251a94c6195148c8
dbv111m <dbv111m@gmail.com>
2020-12-09 01:05:26 +0300

    Запуск Django вне самого проекта.

--- src/run_scraping.py
+++ src/run_scraping.py
@@ -1,6 +1,17 @@
 import codecs
+import os, sys
+
+
+
+proj = os.path.dirname(os.path.abspath('manage.py'))
+sys.path.append(proj)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'scraping_service.settings'
+
+import django
+django.setup()
 
 from scraping.myparsers import *
+from scraping.models import City, Language, Vacancy
 
 parsers_ = (
     (hhru, 'https://hh.ru/search/vacancy?clusters=true&area=1&no_magic=true&enable_snippets=true&salary=&st=searchVacancy&text=python'),
@@ -9,7 +20,7 @@ parsers_ = (
     (dou, 'https://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D0%B5%D0%B2&category=Python'),
     (djinni, 'https://djinni.co/jobs/keyword-python/kyiv/'),
 )
-
+city = City.objects.filter(slug = 'kiev')
 jobs, errors = [], []
 for parsed_site, parsed_url in parsers_:
     current_jobs, current_errors = parsed_site(parsed_url)
