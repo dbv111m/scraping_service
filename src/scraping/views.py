963d5d76d0ee47e40c263e4ef6cbdc72763ececa
dbv111m <dbv111m@gmail.com>
2020-12-23 13:17:50 +0300

    Реорганизация функций отображения.

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -8,6 +8,12 @@ from .models import Vacancy
 def home_view(request):
     # print(request.GET) # возвращает словарь
     form = FindForm()
+    return render(request, 'scraping/home.html', { 'form': form})
+
+
+def list_view(request):
+    # print(request.GET) # возвращает словарь
+    form = FindForm()
     city=request.GET.get('city')
     language=request.GET.get('language')
     qs=[]
@@ -16,9 +22,6 @@ def home_view(request):
         if city:
             _filter['city__slug'] = city # в Django обращение идет через __
         if language:
-            _filter['language__slug'] = language # в Django обращение идет через __
-
-
+            _filter['language__slug'] = language
         qs = Vacancy.objects.filter(**_filter)
-
-    return render(request, 'scraping/home.html', {'object_list': qs, 'form': form})
+    return render(request, 'scraping/list.html', {'object_list': qs, 'form': form})
