7693cdbdcc02907c64f81fade37938ee53cf8ea2
dbv111m <dbv111m@gmail.com>
2020-12-29 20:04:42 +0300

    Функции входа / выхода пользователя

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,23 +1,32 @@
-from django.contrib.auth import authenticate, login, forms, logout
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
-from accounts.forms import UserLoginForm
+from accounts.forms import UserLoginForm, UserRegistrationForm
 
 
 def login_view(request):
-    form=UserLoginForm(request.POST or None)
+    form = UserLoginForm(request.POST or None)
     if form.is_valid():
         data = form.cleaned_data
         email = data.get('email')
         password = data.get('password')
-        user = authenticate(request, username=email, password=password)
+        user = authenticate(request, email=email, password=password)
         login(request, user)
-        return redirect ('home')
+        return redirect('home')
     return render(request, 'accounts/login.html', {'form': form})
-
 
 
 def logout_view(request):
     logout(request)
-    return redirect ('home')
+    return redirect('home')
+
+
+def register_view(request):
+    form = UserRegistrationForm(request.POST or None)
+    if form.is_valid():
+        new_user = form.save(commit=False)
+        new_user.set_password(form.cleaned_data['password'])
+        new_user.save()
+        return render(request, 'accounts/register_done.html',
+                      {'new_user':new_user})
+    return render(request, 'accounts/register.html', {'form': form})
