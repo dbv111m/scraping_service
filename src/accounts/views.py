f4fa9e90bf0dc346d028c79582d6285bf844ee54
dbv111m <dbv111m@gmail.com>
2020-12-31 03:22:09 +0300

    Добавлены messages (alerts)

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout, get_user_model
 
 from accounts.forms import UserLoginForm, UserRegistrationForm, UserUpdateForm
+from django.contrib import messages
 
 User = get_user_model()
 
@@ -29,6 +30,7 @@ def register_view(request):
         new_user = form.save(commit=False)
         new_user.set_password(form.cleaned_data['password'])
         new_user.save()
+        messages.success(request, 'Пользователь добавлен в систему.')
         return render(request, 'accounts/register_done.html',
                       {'new_user':new_user})
     return render(request, 'accounts/register.html', {'form': form})
@@ -44,6 +46,7 @@ def update_view(request):
                 user.language = data['language']
                 user.srnd_email = data['srnd_email']
                 user.save()
+                messages.success(request, 'Данные сохранены.')
                 return redirect('accounts:update')
 
         form = UserUpdateForm(
@@ -59,4 +62,5 @@ def delete_view(request):
         if request.method == 'POST':
             qs = User.objects.get(pk=user.pk)
             qs.delete()
+            messages.success(request, 'Пользователь удален.')
     return redirect('home')
