98a9b77afc750a8cfc6b66d4b2b0c09d058038a5
Dan <dbv111m@gmail.com>
2021-02-01 07:54:32 +0300

    Отправка писем по сегодняшней дате

--- src/send_emails.py
+++ src/send_emails.py
@@ -1,5 +1,5 @@
 import os, sys
-
+import datetime
 import django
 from django.contrib.auth import get_user_model
 from django.core.mail import EmailMultiAlternatives
@@ -13,24 +13,16 @@ django.setup()
 
 from scraping.models import Vacancy
 from scraping_service.settings import EMAIL_HOST_USER
-subject = "Рассылка вакансий"
-text_content = "Рассылка вакансий"
-from_email = EMAIL_HOST_USER
-empty = '<h2>К сожалению на сегодня по Вашим предпочтениям данных нет. </h2>'
 
-# Получение списка емайл для рассыдки из базы https://clck.ru/T3KaK
-#
+today = datetime.date.today()
+subject = f"Рассылка вакансий за {today}"
+text_content = f"Рассылка вакансий за {today}"
+from_email = EMAIL_HOST_USER
+empty = '<h2>К сожалению, на сегодня по Вашим предпочтениям данных нет. </h2>'
+
+# Получение словаря со списком емайл для рассыдки из базы данных. Визуализация: https://clck.ru/T3KaK
 User= get_user_model()
 qs = User.objects.filter(srnd_email=True).values('city', 'language', 'email')
-
-''' 
-Получен список словарей <QuerySet [
-{'city': 2, 'language': 1, 'email': ' m@gmail.com'}, 
-{'city': 3, 'language': 1, 'email': 'u@2.ru'}, 
-{'city': 1, 'language': 1, 'email': 'u@3.ru'}
-]>
-'''
-
 users_dct = {}
 for i in qs: # Для каждого словаря в списке qs
     users_dct.setdefault(
@@ -38,16 +30,21 @@ for i in qs: # Для каждого словаря в списке qs
     users_dct[(i['city'], i['language'])].append(i['email'])
 
 if users_dct:
+    # Формирование списка вакансий для тела письма
     params = {'city_id__in': [], 'language_id__in': []}
     for pair in users_dct.keys():
         params['city_id__in'].append(pair[0])
         params['language_id__in'].append(pair[1])
-    qs = Vacancy.objects.filter(**params).values()[:10]
+    qs = Vacancy.objects.filter(**params, timestamp=today).values()
     vacancies = {}
     for i in qs:
         vacancies.setdefault((i['city_id'], i['language_id']), [])
         vacancies[(i['city_id'], i['language_id'])].append(i)
+
+    # Формирование и отправка писем
     for keys, emails in users_dct.items():
+
+        # Формирование HTML для письма
         rows = vacancies.get(keys, [])
         html = ''
         for row in rows:
@@ -55,6 +52,8 @@ if users_dct:
             html += f'<p>{row["description"]} </p>'
             html += f'<p>{row["company"]} </p><br><hr>'
         _html = html if html else empty
+
+        # Отправка письма по списку
         for email in emails:
             to = email
             msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
