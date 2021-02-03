320bac79f2459784872828a3b5f23e1578a9141d
Dan <dbv111m@gmail.com>
2021-02-03 03:37:00 +0300

    Проверка на наличие пар ЯП и Город в runscraping

--- src/run_scraping.py
+++ src/run_scraping.py
@@ -38,13 +38,14 @@ def get_urls(_settings):
     url_dict = {(q['city_id'], q['language_id']): q['url_data'] for q in qs}
     urls = []
     for pair in _settings:
-        tmp = {}
-        tmp['city'] = pair[0]
-        tmp['language'] = pair[1]
-        url_data = url_dict.get(pair)
-        if url_data:
-            tmp['url_data'] = url_dict.get(pair)
-            urls.append(tmp)
+        if pair in url_dict:
+            tmp = {}
+            tmp['city'] = pair[0]
+            tmp['language'] = pair[1]
+            url_data = url_dict.get(pair)
+            if url_data:
+                tmp['url_data'] = url_dict.get(pair)
+                urls.append(tmp)
     return urls
 
 
