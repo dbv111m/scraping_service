9c658b62bb693f34a810ad18dd7b9f87654b50f8
Dan <dbv111m@gmail.com>
2021-02-15 21:35:45 +0300

    Экспорт данных по скрапингу в json

--- src/scraping_service/settings/production.py
+++ src/scraping_service/settings/production.py
@@ -35,8 +35,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+# SECURITY WARNING: keep the secret key used in production secret!SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
